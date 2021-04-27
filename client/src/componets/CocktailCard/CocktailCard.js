6756724459dd438e64808a20a794357ab73f780c
Shahar Eliyahu <shahar.e3@gmail.com>
2021-04-27 20:12:27 +0300

    up

--- client/src/componets/CocktailCard/CocktailCard.js
+++ client/src/componets/CocktailCard/CocktailCard.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { Card } from "antd";
+import "./CocktailCard.css";
+import { useHistory } from "react-router-dom";
+const { Meta } = Card;
 
-const CocktailCard = () => {
-  return <div></div>;
+const CocktailCard = ({ image, name }) => {
+  return (
+    <Card
+      hoverable
+      className="cocktail-card"
+      cover={<img alt={name} src={image} />}
+    >
+      <Meta description={name} />
+    </Card>
+  );
 };
 
 export default CocktailCard;
--- client/src/pages/OneIngredient/OneIngredient.js
+++ client/src/pages/OneIngredient/OneIngredient.js
@@ -2,10 +2,13 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CocktailCard from "../../componets/CocktailCard/CocktailCard";
 import Container from "../../componets/Container";
+import { LeftOutlined } from "@ant-design/icons";
+import { useHistory } from "react-router-dom";
 
 const OneIngredient = () => {
   const [loading, setLoading] = useState(true);
   const [cocktails, setCocktails] = useState([]);
+  const history = useHistory();
 
   const params = useParams();
   useEffect(() => {
@@ -27,11 +30,20 @@ const OneIngredient = () => {
   console.log("object", params);
 
   return (
-    <Container loading={loading}>
-      {cocktails.map(({ name, image, id }) => (
-        <CocktailCard name={name} image={image} id={id} key={id} />
-      ))}
-    </Container>
+    <>
+      <div className="go-back-container">
+        <LeftOutlined
+          style={{ padding: 10, paddingBottom: 20 }}
+          onClick={() => history.goBack()}
+        />
+        <h1>{params.id}</h1>
+      </div>
+      <Container loading={loading}>
+        {cocktails.map(({ name, image, id }) => (
+          <CocktailCard name={name} image={image} id={id} key={id} />
+        ))}
+      </Container>
+    </>
   );
 };
 
