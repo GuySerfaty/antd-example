c51268007c3ac2f2fe3b29050d76a6a1b2b52209
Guy Serfaty <guy@poob.me>
2021-04-27 23:45:20 +0300

    fix second test

--- client/src/tests/main.test.js
+++ client/src/tests/main.test.js
@@ -1,9 +1,10 @@
 const puppeteer = require("puppeteer");
-const nock = require("nock");
-const useNock = require("nock-puppeteer");
-const { takeScreenshot, BASE_CONFIG, delay, BASE_URL } = require("./test.util");
-const { MOCK_INGREDIENTS } = require("./mocks");
-const folderPath = `homepage`;
+const nock = require('nock');
+const useNock = require('nock-puppeteer');
+const { takeScreenshot, BASE_CONFIG, delay, CLIENT_URL } = require("./test.util");
+const folderPath = `main`;
+const { MOCK_INGREDIENTS, MOCK_COCKTAILS } = require("./mocks");
+
 
 let browser;
 let page;
@@ -11,20 +12,47 @@ jest.setTimeout(30000);
 
 describe("Screenshot testing", () => {
   beforeAll(async () => {
-    browser = await puppeteer.launch({ headless: true });
+    browser = await puppeteer.launch({ headless: true,
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+      ]
+    });
     page = await browser.newPage();
-    useNock(page, [BASE_URL]);
+    useNock(page, [`${CLIENT_URL}/api/`])
   });
 
   afterAll(async () => {
     await browser.close();
   });
 
-  it("Take screenshot of home page", async () => {
-    await nock(BASE_URL).get("/api/ingredients").reply(200, MOCK_INGREDIENTS);
-    await page.goto(BASE_URL);
+  it("Should load homepage and pixel perfect", async () => {
+    await nock(CLIENT_URL)
+      .get('/api/ingredients')
+      .reply(200, MOCK_INGREDIENTS);
+    await page.goto(CLIENT_URL);
     await page.setViewport(BASE_CONFIG);
     await delay(3);
-    await takeScreenshot("example", folderPath, page);
+    await takeScreenshot("homepage", folderPath, page);
+  });
+
+  it("Should choose Gin and get his list", async () => {
+    const SELECTED_INGREDIENT = "Gin";
+
+    await nock(CLIENT_URL).get("/api/ingredients").reply(200, MOCK_INGREDIENTS);
+    await page.goto(CLIENT_URL);
+    await page.setViewport(BASE_CONFIG);
+    await delay(3);
+    const selectedIngredientCard = await page.$(
+      `[alt="${SELECTED_INGREDIENT}"]`
+    );
+    await nock(CLIENT_URL)
+      .get(`/api/cocktails`).query({
+        ingredient: SELECTED_INGREDIENT
+      })
+      .reply(200, MOCK_COCKTAILS);
+    await selectedIngredientCard.click();
+    await delay(6);
+    await takeScreenshot("gin-list", folderPath, page);
   });
 });
--- client/src/tests/mocks/index.js
+++ client/src/tests/mocks/index.js
@@ -1,63 +1,64 @@
-export const MOCK_INGREDIENTS = [
-  {
-    name: "Vodka",
-    id: "Vodka",
-    image: "https://www.thecocktaildb.com/images/ingredients/vodka-Small.png",
-  },
-  {
-    name: "Gin",
-    id: "Gin",
-    image: "https://www.thecocktaildb.com/images/ingredients/gin-Small.png",
-  },
-];
-
-export const MOCK_COCKTAILS = [
-  {
-    name: "3-Mile Long Island Iced Tea",
-    image:
-      "https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg",
-    id: "15300",
-  },
-  {
-    name: "69 Special",
-    image:
-      "https://www.thecocktaildb.com/images/media/drink/vqyxqx1472669095.jpg",
-    id: "13940",
-  },
-  {
-    name: "A1",
-    image:
-      "https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg",
-    id: "17222",
-  },
-  {
-    name: "Abbey Cocktail",
-    image:
-      "https://www.thecocktaildb.com/images/media/drink/mr30ob1582479875.jpg",
-    id: "17834",
-  },
-  {
-    name: "Abbey Martini",
-    image:
-      "https://www.thecocktaildb.com/images/media/drink/2mcozt1504817403.jpg",
-    id: "17223",
-  },
-  {
-    name: "Ace",
-    image:
-      "https://www.thecocktaildb.com/images/media/drink/l3cd7f1504818306.jpg",
-    id: "17225",
-  },
-  {
-    name: "Adam & Eve",
-    image:
-      "https://www.thecocktaildb.com/images/media/drink/vfeumw1504819077.jpg",
-    id: "17226",
-  },
-  {
-    name: "Addison",
-    image:
-      "https://www.thecocktaildb.com/images/media/drink/yzva7x1504820300.jpg",
-    id: "17228",
-  },
-];
+module.exports = {
+  MOCK_INGREDIENTS: [
+    {
+      name: "Vodka",
+      id: "Vodka",
+      image: "https://www.thecocktaildb.com/images/ingredients/vodka-Small.png",
+    },
+    {
+      name: "Gin",
+      id: "Gin",
+      image: "https://www.thecocktaildb.com/images/ingredients/gin-Small.png",
+    },
+  ],
+  MOCK_COCKTAILS: [
+    {
+      name: "3-Mile Long Island Iced Tea",
+      image:
+        "https://www.thecocktaildb.com/images/media/drink/rrtssw1472668972.jpg",
+      id: "15300",
+    },
+    {
+      name: "69 Special",
+      image:
+        "https://www.thecocktaildb.com/images/media/drink/vqyxqx1472669095.jpg",
+      id: "13940",
+    },
+    {
+      name: "A1",
+      image:
+        "https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg",
+      id: "17222",
+    },
+    {
+      name: "Abbey Cocktail",
+      image:
+        "https://www.thecocktaildb.com/images/media/drink/mr30ob1582479875.jpg",
+      id: "17834",
+    },
+    {
+      name: "Abbey Martini",
+      image:
+        "https://www.thecocktaildb.com/images/media/drink/2mcozt1504817403.jpg",
+      id: "17223",
+    },
+    {
+      name: "Ace",
+      image:
+        "https://www.thecocktaildb.com/images/media/drink/l3cd7f1504818306.jpg",
+      id: "17225",
+    },
+    {
+      name: "Adam & Eve",
+      image:
+        "https://www.thecocktaildb.com/images/media/drink/vfeumw1504819077.jpg",
+      id: "17226",
+    },
+    {
+      name: "Addison",
+      image:
+        "https://www.thecocktaildb.com/images/media/drink/yzva7x1504820300.jpg",
+      id: "17228",
+    },
+  ]
+}
