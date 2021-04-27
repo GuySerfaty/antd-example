962d4d5c058c417aa3917bb85e1cecbc5cbbc8de
Guy Serfaty <guy@poob.me>
2021-04-27 23:56:47 +0300

    Add header

--- client/src/pages/IngredientsList/IngredientsList.js
+++ client/src/pages/IngredientsList/IngredientsList.js
@@ -2,6 +2,12 @@ import React, { useEffect, useState } from "react";
 import IngredientCard from "../../componets/IngredientCard/IngredientCard";
 import Container from "../../componets/Container";
 
+const Footer = () => (
+  <div style={{ position: 'absolute', bottom: 0, width: '100%', textAlign: 'center' }}>
+    <h2>THIS IS THE BEST FOOTER IN 2021</h2>
+  </div>
+)
+
 const IngredientsList = () => {
   const [loading, setLoading] = useState(true);
   const [ingredientsList, setIngredientsList] = useState([]);
@@ -20,11 +26,14 @@ const IngredientsList = () => {
   }, []);
 
   return (
-    <Container loading={loading}>
-      {ingredientsList.map(({ name, image, id }) => (
-        <IngredientCard key={id} image={image} id={id} name={name} />
-      ))}
-    </Container>
+    <>
+      <Container loading={loading}>
+        {ingredientsList.map(({ name, image, id }) => (
+          <IngredientCard key={id} image={image} id={id} name={name} />
+        ))}
+      </Container>
+      <Footer />
+    </>
   );
 };
 
